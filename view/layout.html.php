433f146bde2ca668d2c7b2d260b45f7065587f15
Mattia TORSELLO <mtorsell@e2r7p11.42.fr>
2016-04-15 22:53:49 +0200

    bases of layout

---
+++ view/layout.html.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="<?php WEBROOT ?>/web/css">
+        <title>no ski tue le ski</title>
+    </head>
+    <body>
+        <header>Header ici</header>
+        <footer>Footer ici</footer>
+    </body>
+</html>
