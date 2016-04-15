d8158d82e63d57bc22826cf75d1823c84e777daa
Snosky <tpayen@student.42.fr>
2016-04-15 23:18:20 +0200

    view

---
+++ view/index_form.html.php
@@ -0,0 +1,41 @@
+<main class="form-container">
+    <div id="form-container-left">
+        <h1>Login</h1>
+        <form class="login-form">
+            <ul>
+                <li>
+                    <label>Username :</label><br/>
+                    <input type="text" name="name" value="">
+                </li>
+                <li>
+                    <label>Password :</label><br/>
+                    <input type="password" name="password" value="">
+                </li>
+                <li>
+                    <input type="submit" name="submit" value="log in">
+                </li>
+            </ul>
+
+            <a href="#">Forgot your password ?</a>
+        </form>
+    </div>
+    <div id="form-container-right">
+        <h1>New here ?</h1>
+        <ul>
+            <li>
+                <label>Choose a username :</label><br/>
+                <input type="text" name="name" value="">
+            </li>
+            <li><label>Choose a password :</label><br/>
+                <input type="password" name="password" value=""></li>
+            <li><label>Retype your password :</label><br/>
+                <input type="password" name="password-check" value=""></li>
+            <li><label>Your e-mail :</label><br/>
+                <input type="email" name="email" value=""></li>
+            <li><label>Retype your e-mail :</label><br/>
+                <input type="email" name="email-check" value=""></li>
+            <li>
+                <input type="submit" name="register" value="register"></li>
+        </ul>
+    </div>
+</main>
--- view/layout.html.php
+++ view/layout.html.php
@@ -1,9 +1,13 @@
 <!DOCTYPE html>
-<html>
+<<<<<<< HEAD
+<html lang="en">
 <head>
-    
+    <meta charset="UTF-8">
+    <title>Swag Alert!!</title>
 </head>
 <body>
+    <header>Header here</header>
+    <footer>Footer here</footer>
     <?php echo $content_for_layout ?>
 </body>
 </html>
