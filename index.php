063efd06412c5d598456e9312bb9e5fb62c5a11f
Snosky <tpayen@student.42.fr>
2016-04-16 01:29:38 +0200

    WEBROOT

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ session_start();
 
 define('ROOT', str_replace('index.php', '', $_SERVER['SCRIPT_FILENAME']));
 define('DS', DIRECTORY_SEPARATOR);
-define('WEBROOT', str_replace('index.php', '', $_SERVER['SCRIPT_NAME']));
+define('WEBROOT', str_replace('index.php', '', $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'].$_SERVER['SCRIPT_NAME']));
 
 function __autoload($classname)
 {
