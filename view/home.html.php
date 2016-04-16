f04238508d9b7c420c864ddf48684c8c5b1e72dc
Mattia TORSELLO <mtorsell@e2r7p11.42.fr>
2016-04-16 02:29:48 +0200

    salut

---
+++ view/home.html.php
@@ -0,0 +1,12 @@
+<main>
+    <div class="buttons">
+        Create<br/>Join<br/><div>lore</div>
+    </div>
+    <div class="rules">
+        <h1>Rules</h1>
+        <p>No slurs except racists ones.</p>
+    </div>
+    <div id="chat-container">
+        
+    </div>
+</main>
