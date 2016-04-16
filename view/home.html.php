36dc86ac9d475557bf21d72caf145f761e464e63
Mattia TORSELLO <mtorsell@e2r7p11.42.fr>
2016-04-16 06:29:51 +0200

    Home page started, not really responsive

--- view/home.html.php
+++ view/home.html.php
@@ -1,12 +1,31 @@
-<main>
-    <div class="buttons">
-        Create<br/>Join<br/><div>lore</div>
-    </div>
-    <div class="rules">
-        <h1>Rules</h1>
-        <p>No slurs except racists ones.</p>
+<main id="home-container">
+    <div class="left-home-container">
+        <div class="left-top-home-part">
+            <div class="lore-link">
+                <a href="#">
+                    -Lore-
+                </a>
+            </div>
+            <div class="game-launcher">
+                <a href="#" class="launcher-button">Create</a>
+                <a href="#" class="launcher-button">Join</a>
+            </div>
+        </div>
+        <div class="rules">
+            <h1>-Rules-</h1>
+            <p>Treat your opponents as you would treat your mother.</p>
+            <p>Don't be a crybaby, losing is a part of the game just as much as winnning</p>
+            <p>No aimbot</p>
+            <p>Bug exploiting is allowed as long as you don't use a third party program to do it.</p>
+        </div>
     </div>
     <div id="chat-container">
-        
+        <div id="inner-chat-container">
+            <div id="chat-messages-display">
+                <span class="chat-sender">Matt</span><span class="chat-message">: Wesh !<br/></div>
+            <div id="chat-send-container">
+                <input type="text"><input type="submit">
+            </div>
+        </div>
     </div>
 </main>
