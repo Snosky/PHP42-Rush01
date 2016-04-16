bc455f71d57b59ffa722567968370ead46060fc8
Snosky <tpayen@student.42.fr>
2016-04-16 18:00:20 +0200

    Chat

---
+++ Domain/ChatMessage.class.php
@@ -0,0 +1,89 @@
+<?php
+namespace Domain;
+
+class ChatMessage
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $content;
+
+    /**
+     * @var User
+     */
+    private $user;
+
+    /**
+     * @var integer
+     */
+    private $chat_id;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param User $user
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * @return int
+     */
+    public function getChatId()
+    {
+        return $this->chat_id;
+    }
+
+    /**
+     * @param int $chat_id
+     */
+    public function setChatId($chat_id)
+    {
+        $this->chat_id = $chat_id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param string $content
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+    }
+}
