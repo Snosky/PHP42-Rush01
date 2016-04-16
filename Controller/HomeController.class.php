235066bed5d05f67d063d69d7f06ea37798c8b24
Snosky <tpayen@student.42.fr>
2016-04-16 02:30:15 +0200

    remove debug

--- Controller/HomeController.class.php
+++ Controller/HomeController.class.php
@@ -6,9 +6,6 @@ class HomeController extends Controller
     public function homeAction()
     {
         $this->loadModel('UserModel');
-
-        echo '<pre>';
-        print_r($this->UserModel->findAll());
         $this->render('home');
     }
 }
