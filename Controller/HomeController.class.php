fe95cf69442516329e35ef3c6826e1dbb1ef3d2a
Mattia TORSELLO <mtorsell@e2r8p12.42.fr>
2016-04-17 19:35:10 +0200

    lore controller implemented

--- Controller/HomeController.class.php
+++ Controller/HomeController.class.php
@@ -14,4 +14,9 @@ class HomeController extends Controller
             'chatMessages'   => $chatMessages,
         ));
     }
+
+    public function loreAction()
+    {
+        $this->render('lore');
+    }
 }
