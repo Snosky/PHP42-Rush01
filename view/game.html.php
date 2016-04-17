038ef5c84bafbb595e5461acd33e4e44a77422e7
Mattia TORSELLO <mtorsell@e2r8p12.42.fr>
2016-04-17 22:36:31 +0200

    bouton delete

--- view/game.html.php
+++ view/game.html.php
@@ -1,12 +1,15 @@
-<div class="ingame_display">
-</div>
-<div class="ingame_chat">
-	<p class="ingame_chat_title">Welcome to <span class="bold"><?php echo $game->getAdmin()->getUsername(); ?></span>'s game!<br><br>
-	</p>
-	<p>Online Players :<BR><BR></p>
-    <?php foreach ($game->getPlayers() as $player) : ?>
-        <p><?php echo $player->getUsername(); ?></p>
-    <?php endforeach; ?>	
-	<iframe src="<?php echo WEBROOT ?>chat/home/<?php echo $game->getId() ?>" frameborder="0"
-	class="yoloswag"></iframe>
-</div>
+<main>
+	<div class="ingame_display">
+	</div>
+	<a href="<?php echo (WEBROOT."/game/leave/".$game->getId());?>
+	<div class="ingame_chat">
+		<p class="ingame_chat_title">Welcome to <span class="bold"><?php echo $game->getAdmin()->getUsername(); ?></span>'s game!<br><br>
+		</p>
+		<p>Online Players :<BR><BR></p>
+		<?php foreach ($game->getPlayers() as $player) : ?>
+			<p><?php echo $player->getUsername(); ?></p>
+		<?php endforeach; ?>
+		<iframe src="<?php echo WEBROOT ?>chat/home/<?php echo $game->getId() ?>" frameborder="0"
+		class="yoloswag"></iframe>
+	</div>
+</main>
--- view/tic_tac_toe.html.php
+++ view/tic_tac_toe.html.php
@@ -1,10 +1,10 @@
 <main>
-    <h1>It's <span id="whosturn"><?php echo $game[$id]->turn;?></span>'s turn.</h1>
+    <h1>It's <span id="whosturn"><?php echo $tictactoe->turn;?></span>'s turn.</h1>
     <table id="board">
         <tr>
             <td id="0"><?php
-                if (isset($game[$id])){
-                    if ($game[$id]->$map[0] == 'x')
+                if (isset($tictactoe->$map[0])){
+                    if ($tictactoe->$map[0] == 'x')
                         echo ("x");
                     else
                         echo ('o');
@@ -12,16 +12,16 @@
             </td>
             <td id="1">
                 <?php
-                if (isset($game[$id])){
-                    if ($game[$id]->$map[1] == 'x')
+                if (isset($tictactoe->$map[1])){
+                    if ($tictactoe->$map[1] == 'x')
                         echo ("x");
                     else
                         echo ('o');
                 }?>
             </td>
             <td id="2"><?php
-                if (isset($game[$id])){
-                    if ($game[$id]->$map[2] == 'x')
+                if (isset($tictactoe->$map[2])){
+                    if ($tictactoe->$map[2] == 'x')
                         echo ("x");
                     else
                         echo ('o');
@@ -29,22 +29,22 @@
         </tr>
         <tr>
             <td id="3"><?php
-                if (isset($game[$id])){
-                    if ($game[$id]->$map[3] == 'x')
+                if (isset($tictactoe->$map[3])){
+                    if ($tictactoe->$map[3] == 'x')
                         echo ("x");
                     else
                         echo ('o');
                 }?></td>
             <td id="4"><?php
-                if (isset($game[$id])){
-                    if ($game[$id]->$map[4] == 'x')
+                if (isset($tictactoe->$map[4])){
+                    if ($tictactoe->$map[4] == 'x')
                         echo ("x");
                     else
                         echo ('o');
                 }?></td>
             <td id="5"><?php
-                if (isset($game[$id])){
-                    if ($game[$id]->$map[5] == 'x')
+                if (isset($tictactoe->$map[5])){
+                    if ($tictactoe->$map[5] == 'x')
                         echo ("x");
                     else
                         echo ('o');
@@ -52,22 +52,22 @@
         </tr>
         <tr>
             <td id="6"><?php
-                if (isset($game[$id])){
-                    if ($game[$id]->$map[6] == 'x')
+                if (isset($tictactoe->$map[6])){
+                    if ($tictactoe->$map[6] == 'x')
                         echo ("x");
                     else
                         echo ('o');
                 }?></td>
             <td id="7"><?php
-                if (isset($game[$id])){
-                    if ($game[$id]->$map[7] == 'x')
+                if (isset($tictactoe->$map[7])){
+                    if ($tictactoe->$map[7] == 'x')
                         echo ("x");
                     else
                         echo ('o');
                 }?></td>
             <td id="8"><?php
-                if (isset($game[$id])){
-                    if ($game[$id]->$map[8] == 'x')
+                if (isset($tictactoe->$map[8])){
+                    if ($tictactoe->$map[8] == 'x')
                         echo ("x");
                     else
                         echo ('o');
